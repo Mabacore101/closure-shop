113372fe846de1d93c1eabebbac423e60fcd5e1c
Mabacore101 <valentinozhuo2@gmail.com>
2024-09-09 10:08:52 +0700

    Memperbarui nama di views.py

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'nama_aplikasi' : 'Closure Shop',
-        'nama': 'Valentino Vieri ZHuo',
+        'nama': 'Valentino Vieri Zhuo',
         'kelas': 'PBP E'
     }
 
